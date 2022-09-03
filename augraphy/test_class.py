f9b343bb8fcd1e14da1b9820150db0d687344a62
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-09-03 23:28:46 +0800

    Added testing file.

---
+++ augraphy/test_class.py
@@ -0,0 +1,16 @@
+
+class test_class123(object):
+    """This is a testing class
+
+    :param abc: test 1
+    :type abc: int, optional
+    :param bbb: test2
+    :type int optional
+    """
+
+    def __init__(self, abc=0, bbb=0):
+        """Constructor method
+        """
+
+        self=abc = abc
+        self.bbb=bbb
