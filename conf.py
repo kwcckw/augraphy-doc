0c8ccfc2469b07cd2f20b36ffff91ee650cd09db
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2023-05-14 20:53:52 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -11,9 +11,9 @@
 import os
 import sys
 
-
+sys.path.insert(0, os.path.abspath(os.getcwd()))
 sys.path.insert(0, os.path.abspath("/augraphy"))
-sys.path.insert(0, os.path.abspath("/"))
+
 
 # -- Project information -----------------------------------------------------
 
